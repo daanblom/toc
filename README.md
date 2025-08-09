82f8ee8c9c932e028236c1ec3be363ba10c963f1
daan <contact@daanblom.com>
2025-08-09 11:56:31 +0200

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# toc
+
+Creates a table of content (including links) either from a file (current version), or trough a lua plugin in (neo)vim (planned)
---
+++ toc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+file="$1"
+
+extractTitles() {
+    local file="$1"
+    local titles=()
+    local indent=" "
+    local prefix=""
+
+    while IFS= read -r line; do
+        if [[ "$line" =~ ^# ]]; then
+          if [[ $(echo "$line" | grep -c "^# ") -eq 1 ]]; then
+                indent=""
+                prefix="- "
+              elif [[ $(echo "$line" | grep -c "^## ") -eq 1 ]]; then
+                indent=" "
+                prefix="* "
+              elif [[ $(echo "$line" | grep -c "^### ") -eq 1 ]]; then
+                indent="  "
+                prefix="* "
+            else
+                indent=""
+                prefix=""
+            fi
+            title=$(echo "$line" | sed 's/^#* //')
+            Fulltitle=$(echo "$line")
+            echo "$indent$prefix[$title](#$(echo $title | sed 's/ /-/g'))"
+        fi
+    done < "$file"
+}
+
+extractTitles "$file"
