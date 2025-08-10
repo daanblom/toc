5d26f898e55242f31be9998a7c7c23e10236ba75
daan <contact@daanblom.com>
2025-08-10 11:20:54 +0200

    Rewrite function

--- toc.sh
+++ toc.sh
@@ -16,37 +16,29 @@ fileCheck () {
 
 extractTitles() {
     local file="$1"
-    local titles=()
-    local indent=" "
-    local prefix=""
+    local line level title indent prefix
 
     while IFS= read -r line; do
-        if [[ "$line" =~ ^# ]]; then
-          if [[ $(echo "$line" | grep -c "^# ") -eq 1 ]]; then
-                indent=""
+        if [[ $line =~ ^(#{1,6})[[:space:]]+(.*) ]]; then
+            level=${#BASH_REMATCH[1]}      # heading level = number of '#'
+            title=${BASH_REMATCH[2]}       # actual heading text
+
+            # Indent: 0 for h1, 1 space for h2, etc.
+            indent=$(printf '%*s' $((level - 1)) '')
+
+            # Prefix: dash for h1, asterisk otherwise
+            if (( level == 1 )); then
                 prefix="- "
-              elif [[ $(echo "$line" | grep -c "^## ") -eq 1 ]]; then
-                indent=" "
-                prefix="* "
-              elif [[ $(echo "$line" | grep -c "^### ") -eq 1 ]]; then
-                indent="  "
-                prefix="* "
-              elif [[ $(echo "$line" | grep -c "^#### ") -eq 1 ]]; then
-                indent="   "
-                prefix="* "
-              elif [[ $(echo "$line" | grep -c "^##### ") -eq 1 ]]; then
-                indent="    "
-                prefix="* "
-              elif [[ $(echo "$line" | grep -c "^###### ") -eq 1 ]]; then
-                indent="     "
-                prefix="* "
             else
                 indent=""
                 prefix=""
             fi
-            title=$(echo "$line" | sed 's/^#* //')
-            Fulltitle=$(echo "$line")
-            echo "$indent$prefix[$title](#$(echo $title | sed 's/ /-/g'))"
+
+            # Anchor: lowercase, spaces -> '-', remove non-alphanum except '-'
+            anchor=$(echo "$title" | tr '[:upper:]' '[:lower:]' \
+                     | sed -E 's/[^a-z0-9 -]//g; s/[[:space:]]+/-/g')
+
+            echo "${indent}${prefix}[${title}](#${anchor})"
         fi
     done < "$file"
 }
